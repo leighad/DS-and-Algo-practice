58767c36d80e5d5f21bb6c09f555285c98f3d97c
Leigha <>
2021-06-10 01:21:08 +0000

    add length of last word method

--- main.rb
+++ main.rb
@@ -1,3 +1,14 @@
+# Given a string s that consists of some words separated by spaces, 
+# return the length of the last word in the string. 
+# If the last word does not exist, return 0.
+# A word is a maximal substring consisting of non-space characters only.
+
+def length_of_last_word(s)
+    split_string = s.split
+    split_string[-1] ? split_string[-1].length : 0
+end
+
+
 def two_sum(nums, target)
   nums.each_with_index do |num, idx|
     start = nums[0]
